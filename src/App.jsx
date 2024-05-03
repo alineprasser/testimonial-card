20f4896d29687b36f3a928cd1ea7a863447cadfb
Aline Prasser <aline.prasser@codenapp.com>
2024-05-02 23:02:52 -0300

    feat: add styles and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <div className="card-wrapper">
+        <div className="card-profile">
+          <img
+            src="src/assets/profile-thumbnail.png"
+            alt="User profile picture"
+          />
+          <div className="card-user-name">
+            <h1>Sarah Dole</h1>
+            <span>@sarahdole</span>
+          </div>
+        </div>
+        <p>
+          I&apos;ve been searching for high-quality abstract images for my
+          design projects, and I&apos;m thrilled to have found this platform.
+          The variety and depth of creativity are astounding!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
